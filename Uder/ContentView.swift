f058372f1faff4757e38542dd5071d03d9dc116d
Javier <javelie@outlook.com>
2019-11-27 19:38:27 -0700

    Initial Commit

---
+++ Uder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Uder
+//
+//  Created by José Javier Cueto Mejía on 27/11/19.
+//  Copyright © 2019 José Javier Cueto Mejía. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
